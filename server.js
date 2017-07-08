3976aaf83a79a89569e4da2ff202375141efe36d
steven <stevenle2011@gmail.com>
2017-07-08 12:52:50 +0700

    add heroku test build

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+//server
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
 app.use(morgan('dev'));
 var port = process.env.PORT || 3000;
 
+//database
+var mongoose = require('mongoose');
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/stockData'
+var promise = mongoose.connect(url,{
+	useMongoClient:true,
+})
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error'));
+db.once('open',function(){
+	console.log('Connected correctly to server');
+})
+
+//routers
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
